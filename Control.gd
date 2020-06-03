31e15d01f3d1cf6a3b1b11280ffb00e946fc98bd
idahdam <40880027+idahdam@users.noreply.github.com>
2020-06-04 01:10:24 +0700

    Add files via upload

---
+++ Control.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event):
+	if event.is_action_pressed("pause"):
+		get_tree().paused = not get_tree().paused
+		visible = not visible
